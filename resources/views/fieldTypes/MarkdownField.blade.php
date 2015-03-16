33d4fef21fbc7e2b78922b04d58ed7f8369d951b
David Hemphill <davidlee.hemphill@gmail.com>
2015-03-16 09:59:52 -0500

    View adjustments

--- resources/views/fieldTypes/MarkdownField.blade.php
+++ resources/views/fieldTypes/MarkdownField.blade.php
@@ -1,5 +1,6 @@
 <div class="control g-row">
-    <div class="g-col-1">
+    <a href="#" class="media-browser-open _media-browser-open br2 bg1 uppercase ft1 ls1 fwbold tcw dib">Media Browser</a>
+<div class="g-col-1">
         {!! BumbleForm::label($field->getColumn(), $field->getTitle(), ['class' => 'form__textarea-label '.$model->getRequiredClass($field)]) !!}
         @if ($field->getDescription()) <p class="help-text">{{ $field->getDescription() }}</p> @endif
     </div>
